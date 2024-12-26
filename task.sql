7f1e13d0682e0701b35eb72957ccd3d62fc07cfb
Mykhailo <ivchenkomykhailo@gmail.com>
2024-12-26 19:56:36 +0200

    Solution

--- task.sql
+++ task.sql
@@ -1,11 +1,19 @@
 -- Use our database
 USE ShopDB; 
 
--- Some data should be created outside the transaction (here)
+INSERT INTO Orders (CustomerID, Date)
+VALUES (1, CURRENT_DATE);
+
+SET @order_id = LAST_INSERT_ID();
 
 -- Start the transaction 
-START TRANSACTION; 
+START TRANSACTION;
 
--- And some data should be created inside the transaction 
+INSERT INTO OrderItems (OrderID, ProductID, Count)
+VALUES (@order_id, 1, 1);
 
-COMMIT; 
+UPDATE Products
+SET WarehouseAmount = WarehouseAmount - 1
+WHERE ID = 1;
+
+COMMIT;
